d5035d183db810c80beb211f36e60def147c4936
tiredpixel <tiredpixel@posteo.de>
2015-12-06 19:45:19 +0100

    Fix warning test ambiguous / .

--- test/bank-account-statement_test.rb
+++ test/bank-account-statement_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 describe "BankAccountStatement::VERSION" do
 
   it "uses major.minor.patch" do
-    ::BankAccountStatement::VERSION.must_match /\A\d+\.\d+\.\d+\z/
+    ::BankAccountStatement::VERSION.must_match(/\A\d+\.\d+\.\d+\z/)
   end
 
 end
