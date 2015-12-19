b88fd21927a9f961ad95358a5bce68f9fb5c16c6
tiredpixel <tiredpixel@posteo.de>
2015-12-19 21:30:05 +0100

    [#5] Refactor test Inputs base_test.rb .

--- test/bank-account-statement/inputs/base_test.rb
+++ test/bank-account-statement/inputs/base_test.rb
@@ -1,11 +1,29 @@
 require 'bigdecimal'
 require 'yaml'
 
-require_relative '../../../test_helper'
-require_relative '../../../../lib/bank-account-statement/inputs'
+require_relative '../../test_helper'
+require_relative '../../../lib/bank-account-statement/inputs'
 
 
 {
+  BankAccountStatement::Inputs::HTML::CPBKGB22::Personal::CreditCard::V_2011_04_09 => [
+    'HTML/CPBKGB22/Personal/CreditCard/2011-04-09.html',
+  ],
+  BankAccountStatement::Inputs::HTML::CPBKGB22::Personal::CreditCard::V_2015_05_27 => [
+    'HTML/CPBKGB22/Personal/CreditCard/2015-05-27.html',
+  ],
+  BankAccountStatement::Inputs::HTML::CPBKGB22::Personal::Current::V_2011_05_07 => [
+    'HTML/CPBKGB22/Personal/Current/2011-05-07.html',
+  ],
+  BankAccountStatement::Inputs::HTML::CPBKGB22::Personal::Current::V_2015_03_03 => [
+    'HTML/CPBKGB22/Personal/Current/2015-03-03.html',
+  ],
+  BankAccountStatement::Inputs::HTML::CPBKGB22::Personal::Savings::V_2011_05_07 => [
+    'HTML/CPBKGB22/Personal/Savings/2011-05-07.html',
+  ],
+  BankAccountStatement::Inputs::HTML::CPBKGB22::Personal::Savings::V_2015_03_03 => [
+    'HTML/CPBKGB22/Personal/Savings/2015-03-03.html',
+  ],
   BankAccountStatement::Inputs::TXT::CPBKGB22::Business::Current::V_2015_12_06 => [
     'TXT/CPBKGB22/Business/Current/2015-12-06.txt',
   ],
@@ -13,10 +31,11 @@ require_relative '../../../../lib/bank-account-statement/inputs'
   describe input_klass.name do
     fixtures.each do |fixture|
       it fixture.to_s do
-        f = File.expand_path("../../#{fixture}", __FILE__)
+        f = File.expand_path("../#{fixture}", __FILE__)
         y = "#{f}.yaml"
         
         begin
+          p f ###tp
           fc = File.read(f)
         rescue Errno::ENOENT
           skip # this fixture isn't included because of concerns about copyright
